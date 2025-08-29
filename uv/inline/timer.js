2a7088d4d36ed74a410f1d685cde17767ccb76ca
Tim Cuthbertson <tim@gfxmonk.net>
2025-08-30 09:46:42 +1000

    TimC hacky import of TimW/libuv

--- uv/inline/timer.js
+++ uv/inline/timer.js
@@ -1,11 +1,3 @@
-/*----------------------------------------------------------------------------
-   Copyright 2024, Koka-Community Authors
-
-   Licensed under the MIT License ("The License"). You may not
-   use this file except in compliance with the License. A copy of the License
-   can be found in the LICENSE file at the root of this distribution.
-----------------------------------------------------------------------------*/
-
 function _init_timer(){
   return {};
 }
